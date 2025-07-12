6abb0652bad8f4c47ae223b29806bb90c0f9308f
fberte2k <fberte2k@gmail.com>
2025-07-12 14:29:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
-import HeroSection from "./HeroSection";
-import AboutNOOS from "./AboutNOOS";
-import EcosystemOverview from "./EcosystemOverview";
-import Manifesto from "./Manifesto";
+import React from 'react';
+import HeroSection from './HeroSection';
+import AboutNOOS from './AboutNOOS';
+import EcosystemOverview from './EcosystemOverview';
+import Manifesto from './Manifesto';
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <AboutNOOS />
+      <EcosystemOverview />
+      <Manifesto />
+    </>
+  );
+}
+
+export default App;
