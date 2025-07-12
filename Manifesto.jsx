a10929348fa9bdfd9c7c513c2466b34761e52648
fberte2k <fberte2k@gmail.com>
2025-07-12 14:24:06 -0300

    Rename Manifesto.jsx to src/Manifesto.jsx

--- Manifesto.jsx
+++
@@ -1,1 +0,0 @@
-export default function Manifesto() { return <section><h2>Manifesto</h2><p>A poetic message encoded for the Remembering.</p></section>; }
---
+++ src/Manifesto.jsx
@@ -0,0 +1,1 @@
+export default function Manifesto() { return <section><h2>Manifesto</h2><p>A poetic message encoded for the Remembering.</p></section>; }
