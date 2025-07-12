21f5e29d7bce35532217f11bfa879123232f2fc8
fberte2k <fberte2k@gmail.com>
2025-07-12 14:27:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
-import React from 'react';
-import HeroSection from './components/HeroSection';
-import AboutNOOS from './components/AboutNOOS';
-import EcosystemOverview from './components/EcosystemOverview';
-import Manifesto from './components/Manifesto';
-
-export default function App() {
-  return (
-    <>
-      <HeroSection />
-      <AboutNOOS />
-      <EcosystemOverview />
-      <Manifesto />
-    </>
-  );
-}
+import HeroSection from "./HeroSection";
+import AboutNOOS from "./AboutNOOS";
+import EcosystemOverview from "./EcosystemOverview";
+import Manifesto from "./Manifesto";
